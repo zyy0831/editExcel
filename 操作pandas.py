0a4b85f251d3638f61f58b5ac27ffa1b3ef5640d
zyy0831 <798206656@qq.com>
2020-03-02 14:21:43 +0800

    求和

--- 操作pandas.py
+++ 操作pandas.py
@@ -3,10 +3,29 @@ import pandas as pd
 import numpy as np
 import xlrd,os,xlwt
 from openpyxl import load_workbook
+    
+def Deal(fileName,fileName_Path,sheetNameAll):
+    filename_head = fileName.split('.')[0]
+    exportName=filename_head+'Deal.xlsx'
+    outPath = os.path.join(pathout, exportName)#拼接路径
+    writer = pd.ExcelWriter(outPath)		# 写入Excel文件
+    
+    for i in sheetNameAll:
+        df = pd.read_excel(fileName_Path,sheet_name=i)   
+        Types=['AQI','PM2.5','PM2.5_24h','PM10','PM10_24h','SO2','SO2_24h','NO2','NO2_24h','O3','O3_24h','O3_8h','O3_8h_24h','CO','CO_24h']
+        for Type in Types: 
+            outfile = df[(df['type']==Type)]
+            outfile.loc[Type+'Row_sum'] = outfile.apply(lambda x: x.sum()) #求和
+            # outfile.loc[Type+'Row_mean'] = outfile.apply(lambda x: x.mean()) #求均
+            outfile.to_excel(writer, Type)		# ‘AQI’是写入excel的sheet名
+            # saveExcel(fileName,outfile,Type)
+    writer.save()
+    writer.close()
 
-def getTable(fileName_Path):
+def getTable(fileName,fileName_Path):
     f = pd.ExcelFile(fileName_Path) #读取excel文件
-    table=f.sheet_names[0]  # 获取所有工作表sheet名称 f.sheet_names[0]
+    sheetNameAll=f.sheet_names  # 获取所有工作表sheet名称
+    Deal(fileName,fileName_Path,sheetNameAll)
 
 def getExcelName(data_path):
     for fileName in os.listdir(data_path):
@@ -14,9 +33,7 @@ def getExcelName(data_path):
             #print(fileName)
             fileName_Path = os.path.join(data_path, fileName)#拼接路径
             #print(fileName_Path)
-            getTable(fileName_Path)
-
-
+            getTable(fileName,fileName_Path)
 
 if __name__=="__main__":
     path = r'c:\Users\zyy\Desktop\气象\城市_20190101-20191231-xlsx\新建文件夹'
@@ -24,3 +41,4 @@ if __name__=="__main__":
     pathout = r'c:\Users\zyy\Desktop\气象\城市_20190101-20191231-xlsx\out'
     # pathout = input("请输入输出文件\n")
     getExcelName(path) 
+    print('处理完成')
